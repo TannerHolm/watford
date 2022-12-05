4a75afa9867a72fd57b90b7e19f71a36b4f1d2d2
TannerHolm <holm.tanner@gmail.com>
2022-12-05 13:09:56 -0700

    no separate poster

--- themes/harc/resources/views/template-home.blade.php
+++ themes/harc/resources/views/template-home.blade.php
@@ -5,6 +5,6 @@
 @extends('layouts.app')
 
 @section('content')
-  <img class="cover-img" src="@field('background_video_poster_image', 'url')" alt="">
-  <video width="400px" height="700px" playsinline autoplay="autoplay" loop="true" autoplay="autoplay" muted poster="@field('background_video_poster_image', 'url')" class="home-video" data-src="@field('background_video', 'url')"> </video>
+  {{-- <img class="cover-img" src="@field('background_video_poster_image', 'url')" alt=""> --}}
+  <video width="400px" height="700px" playsinline autoplay="autoplay" loop="true" autoplay="autoplay" muted poster="@field('background_video_poster_image', 'url')" class="home-video" src="@field('background_video', 'url')"> </video>
 @endsection
