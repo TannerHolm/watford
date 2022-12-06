b890752cef8a00cdaaacaf01ed071fe9982631f4
TannerHolm <holm.tanner@gmail.com>
2022-12-05 17:32:56 -0700

    fixed home

--- cache/acorn/framework/views/dfea23e52bdf702d929b8fe17dad538ea2892993.php
+++ cache/acorn/framework/views/dfea23e52bdf702d929b8fe17dad538ea2892993.php
@@ -1,6 +1,9 @@
 <?php $__env->startSection('content'); ?>
   
-  <video width="400px" height="700px" playsinline autoplay="autoplay" loop="true" autoplay="autoplay" muted poster="<?= get_field('background_video_poster_image')['url']; ?>" class="home-video" src="<?= get_field('background_video')['url']; ?>"> </video>
+  <video width="100%" height="100%" playsinline autoplay="autoplay" loop="true" autoplay="autoplay" muted class="home-video">
+    <source src="<?= get_field('background_video')['url']; ?>">
+    <img src="<?= get_field('background_video_poster_image')['url']; ?>" alt="">
+  </video>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/tannerholm/Sites/watford/wp-content/themes/harc/resources/views/template-home.blade.php ENDPATH**/ ?>
--- themes/harc/resources/views/template-home.blade.php
+++ themes/harc/resources/views/template-home.blade.php
@@ -6,5 +6,8 @@
 
 @section('content')
   {{-- <img class="cover-img" src="@field('background_video_poster_image', 'url')" alt=""> --}}
-  <video style="background: white;" width="400px" height="700px" playsinline autoplay="autoplay" loop="true" autoplay="autoplay" muted poster="@field('background_video_poster_image', 'url')" class="home-video" src="@field('background_video', 'url')"> </video>
+  <video width="100%" height="100%" playsinline autoplay="autoplay" loop="true" autoplay="autoplay" muted class="home-video">
+    <source src="@field('background_video', 'url')">
+    <img src="@field('background_video_poster_image', 'url')" alt="">
+  </video>
 @endsection
